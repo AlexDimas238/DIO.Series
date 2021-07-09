84bb36d0943fa02df931a032043a7588b2d60840
Alexandre Dimas <alexdimas238@gmail.com>
2021-07-09 15:55:32 -0300

    Cadastro em memoria
    Cadastro de series em memoria

---
+++ Classes/Filme.cs
@@ -0,0 +1,28 @@
+namespace DIO.Series.Classes
+{
+    public class Filme
+    {
+
+        /* Devo definir uma classe base igual como foi feito na classe serie?
+        
+        Atributos
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+        private bool Excluido { get; set; }
+
+        // Métodos
+
+        public Filme(int Id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+            this.Excluido = false;
+        }
+        */
+    }
+}
